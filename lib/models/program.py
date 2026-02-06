87e408b0493b6cfc990a856b119924e2029a8ed1
margotbourne <margotbourne@icloud.com>
2026-02-06 19:31:41 +0000

    TDD program  model - FOSM  pillars of help eg education

---
+++ lib/models/program.py
@@ -0,0 +1,12 @@
+class Program:
+
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name 
+        self.description = description
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+    
+    def __repr__(self):
+        return f"Supporter({self.id}, {self.name}, {self.description}"
---
+++ tests/test.program.py
@@ -0,0 +1,29 @@
+from lib.models.program import Program
+
+"""
+Program constucts
+"""
+def test_program_constructs():
+    program = Program(1, "Test Name", "Test Description")
+    assert program.id == 1
+    assert program.name == "Test Name"
+    assert program.description == "Test Description"
+
+
+"""
+We can format programs to strings nicely
+"""
+def test_programs_format_nicely():
+    program = Program(1, "Test Name", "Test Description")
+    assert str(program) == "Program(1, Test Name, Test Program)"
+    
+
+"""
+We can compare two identical programs
+And have them be equal
+"""
+def test_programs_are_equal():
+    program1 = Program(1, "Test Name", "Test Description")
+    program2 = Program(1, "Test Name", "Test Description")
+    assert program1 == program2
+   
